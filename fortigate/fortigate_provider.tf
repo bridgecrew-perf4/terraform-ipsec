ac05cf1035909dc5ab0f900716f6334919d0215b
John McDonough <jmcdonough@fortinet.com>
2021-01-27 17:51:11 -0500

    consolidate provider information

--- fortigate/fortigate_provider.tf
+++ fortigate/fortigate_provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    fortios = {
+      source = "fortinetdev/fortios"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
 provider "fortios" {
   hostname = var.fortigate_ip_or_fqdn
   token    = var.fortigate_api_token
--- fortigate/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    fortios = {
-      source = "fortinetdev/fortios"
-    }
-  }
-  required_version = ">= 0.13"
-}
